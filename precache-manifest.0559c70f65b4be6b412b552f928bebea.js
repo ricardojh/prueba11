88c0144686a7a942d082a84dbe62d39b62e746ad
ricardojh05 <ricardojh05@gmail.com>
2020-02-20 16:25:22 -0500

    prueba del commit

---
+++ precache-manifest.0559c70f65b4be6b412b552f928bebea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e90e8789fccbddbaa3c3dfbe397d9d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "742d95c19760581968ab",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7e443683cc1ed106b1ab",
+    "url": "/static/css/main.0a87d574.chunk.css"
+  },
+  {
+    "revision": "742d95c19760581968ab",
+    "url": "/static/js/2.f915fe64.chunk.js"
+  },
+  {
+    "revision": "a52d65c95a2d7ffcb9ff212c4d38dff4",
+    "url": "/static/js/2.f915fe64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e443683cc1ed106b1ab",
+    "url": "/static/js/main.84030823.chunk.js"
+  },
+  {
+    "revision": "b23bf2867ec0bea37c43",
+    "url": "/static/js/runtime-main.3fab922a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
